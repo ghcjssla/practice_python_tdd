27def77e2633b62b0025301e3622fff5782f7036
ghcjssla <mantto2@naver.com>
2016-03-27 10:48:56 +0900

    19장 테이블차리기

--- hello.py
+++ hello.py
@@ -3,24 +3,33 @@ print "Hello Python"
 class TestCase:
 	def __init__(self, name):
 		self.name = name
+	def setUp(self) :
+		pass
 	def run(self):
+		self.setUp()
 		method = getattr(self, self.name)
 		method()
 
+
 class WasRun(TestCase):
-	def __init__(self, name):
-		self.WasRun = None
-		TestCase.__init__(self, name)
 	def testMethod(self):
 		self.WasRun =  1
+	def setUp(self) :
+		self.WasRun = None
+		self.wasSetUp = 1
 	
 
 class TestCaseTest(TestCase) :
+	def setUp(self) :
+		self.test = WasRun("testMethod")
 	def testRunning(self) :
-	      test = WasRun("testMethod")
-	      assert(not test.WasRun)
-	      test.run()
-	      assert(test.WasRun)
+		self.test.run()
+	      	assert(self.test.WasRun)
+	def testSetUp(self) :
+		self.test.run()
+		assert(self.test.wasSetUp)
+	
 TestCaseTest("testRunning").run()
+TestCaseTest("testSetUp").run()
 
 print "is There? test"
